cbc39bc862967c401630cd2d177df2cde7ab9471
saberda <630991493@qq.com>
2020-06-21 22:53:34 -0400

    add function showInfo()

--- EmployeeManagementSystem/src/employee.cpp
+++ EmployeeManagementSystem/src/employee.cpp
@@ -5,3 +5,9 @@ Employee::Employee(int id, string name, int deptId) {
     this->m_Name = name;
     this->m_DeptId = deptId;
 }
+
+void Employee::showInfo() {
+	cout << "Employee ID" << this->m_Id
+		<< " \tEmployee Name " << this->m_Name
+		<< " \tEmployee Job" << this->getDeptName() << endl;
+}
