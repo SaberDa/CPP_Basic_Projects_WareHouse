a4eeeeb9e6349e178cd6d48e35a0f0184960f33d
saberda <630991493@qq.com>
2020-06-21 23:05:46 -0400

    finish manager class

--- EmployeeManagementSystem/src/boss.cpp
+++ EmployeeManagementSystem/src/boss.cpp
@@ -7,3 +7,13 @@ Boss::Boss(int id, string name, int deptId) {
     this->m_Name = name;
     this->m_DeptId = deptId;
 }
+
+void Boss::showInfo() {
+	cout << "Employee ID" << this->m_Id
+		<< " \tEmployee Name " << this->m_Name
+		<< " \tEmployee Job" << this->getDeptName() << endl;
+}
+
+string Boss::getDeptName() {
+    return string("boss");
+}
