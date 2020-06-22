4a03d15f50f504941fedbd80784c6eaf61529064
saberda <630991493@qq.com>
2020-06-21 22:59:31 -0400

    add function Manager()

---
+++ EmployeeManagementSystem/src/manager.cpp
@@ -0,0 +1,10 @@
+/* Implement Manager Class */
+
+#include "manager.h"
+
+Manager::Manager(int id, string name, int deptId) {
+    this->m_Id = id;
+    this->m_Name = name;
+    this->m_DeptId = deptId;
+}
+
