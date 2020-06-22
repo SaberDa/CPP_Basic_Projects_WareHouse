7f686c2d649c8824fba8aaa4d2bc74b6b6694e30
saberda <630991493@qq.com>
2020-06-21 21:46:37 -0400

    init project

---
+++ EmployeeManagementSystem/src/workerManager.h
@@ -0,0 +1,27 @@
+/*
+ * Manager Class
+ * 
+ * Main Functions:
+ * 1. Establish a communication menu interface based on command line with
+ *    users.
+ * 2. Realize the operation of adding, deleting, modifying and checking 
+ *    employees.
+ * 3. Realize reading and writing interaction with files
+*/
+
+#ifndef __WORKERMANAGER_H__
+#define __WORKERMANAGER_H__
+
+#include <iostream>
+
+using namespace std;
+
+class WorkerManager {
+public:
+
+    WorkerManager();
+
+    ~WorkerManager();
+};
+
+#endif
