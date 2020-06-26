bec2167ff5c36967b37b643c84ccb24c5de49338
saberda <630991493@qq.com>
2020-06-25 22:48:31 -0400

    add Speaker class

---
+++ SpeechContestManagementSystem/src/speaker.h
@@ -0,0 +1,19 @@
+/*
+ * Speaker Class
+*/
+
+#ifndef __SPEAKER_H__
+#define __SPEAKER_H__
+
+#include <iostream>
+
+using namespace std;
+
+class Speaker {
+public:
+
+    string m_Name;          // Name
+    double m_Score[2];      // Score
+};
+
+#endif
