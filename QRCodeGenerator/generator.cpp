0f3d4f34c29310baa31d12a7cb0e718d24a3fba8
saberda <630991493@qq.com>
2020-08-21 15:34:36 -0400

    create generator.cpp

---
+++ QRCodeGenerator/generator.cpp
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <climits>
+#include <cstddef>
+#include <cstring>
+#include <sstream>
+#include <stdexcept>
+#include <utility>
+
+#include "generator.hpp"
+
+namespace qrcodegen {
+
+
+
+}
